330759e9753bdc34856926909e7b21addab04de4
wutangyuan <wutangyuan1@gmail.com>
2024-03-25 23:08:27 +0800

    初始化项目文件

---
+++ Windows/AdapteWindow.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace dotnetComponent.Windows
+{
+    /// <summary>
+    /// 用于自适应窗口实现（WPF 版本）
+    /// </summary>
+    public class AdapteWindow : Window
+    {
+    }
+}
